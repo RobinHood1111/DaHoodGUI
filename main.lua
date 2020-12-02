6b4762ff4894273341c3e0408a6555eb5420e044
RobinHood1111 <75328559+RobinHood1111@users.noreply.github.com>
2020-12-02 16:14:21 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,2 @@
 local DaHoodGUIMain = Instance.new("LocalScript")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/RobinHood1111/DaHoodGUI/tree/main/Guis", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RobinHood1111/DaHoodGUI/main/Guis/menu.lua", true))()
