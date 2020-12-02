cc64797b35db062756fe0000bf1f839c50d391a6
RobinHood1111 <75328559+RobinHood1111@users.noreply.github.com>
2020-12-02 16:28:25 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,3 @@
 local DaHoodGUIMain = Instance.new("LocalScript")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/RobinHood1111/DaHoodGUI/main/Guis/menu.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RobinHood1111/DaHoodGUI/main/Scripts/RainbowStroke1", true))()
